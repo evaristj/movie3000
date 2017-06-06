4a0479b757170ced231bcf5bbbbd5f4dfd926b76
Evarist Jaume <evaristjaumet@gmail.com>
2017-06-06 18:23:56 +0200

    todo listo para empezar la página

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'statics/index'
 
   get 'statics/about'
 
   get 'peliculas/index'
   resources :peliculas
+
+  root :to =>"statics#index"
+  get '/index' => 'statics#index', :as => :index
+
+  root :to =>"statics#about"
+  get '/about' => 'statics#about', :as => :about
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
