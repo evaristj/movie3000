8d4b18c27852288ddb6df4b193ec2d830c496ff9
Evarist Jaume <evaristjaumet@gmail.com>
2017-06-14 21:42:06 +0200

    settings

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -55,6 +55,8 @@ ActiveAdmin.setup do |config|
   # This setting changes the method which Active Admin calls
   # within the application controller.
   config.authentication_method = :authenticate_admin_user!
+  #Si es únicamente un directorio /admin por defecto el namespase 
+  #es admin y podríamos dejarlo así. 
 
   # == User Authorization
   #
@@ -62,7 +64,7 @@ ActiveAdmin.setup do |config|
   # method in a before filter of all controller actions to
   # ensure that there is a user with proper rights. You can use
   # CanCanAdapter or make your own. Please refer to documentation.
-  # config.authorization_adapter = ActiveAdmin::CanCanAdapter
+   config.authorization_adapter = ActiveAdmin::CanCanAdapter
 
   # In case you prefer Pundit over other solutions you can here pass
   # the name of default policy class. This policy will be used in every
@@ -70,7 +72,7 @@ ActiveAdmin.setup do |config|
   # config.pundit_default_policy = "MyDefaultPunditPolicy"
 
   # You can customize your CanCan Ability class name here.
-  # config.cancan_ability_class = "Ability"
+   config.cancan_ability_class = "Ability"
 
   # You can specify a method to be called on unauthorized access.
   # This is necessary in order to prevent a redirect loop which happens
