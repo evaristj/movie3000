f5ae4d56b570be7dd7967ce895ac3a61928921ba
Evarist Jaume <evaristjaumet@gmail.com>
2017-06-20 23:40:34 +0200

    columna precio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620174203) do
+ActiveRecord::Schema.define(version: 20170620213928) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170620174203) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "url"
+    t.integer "precio"
   end
 
   create_table "settings", force: :cascade do |t|
