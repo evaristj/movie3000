7575628d7015587e2c0b22bf14a6caf391349030
Evarist Jaume <evaristjaumet@gmail.com>
2017-06-24 12:43:50 +0200

    statics controller

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,23 +1,18 @@
 class StaticsController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
-  	@peliculas = Pelicula.all
 
     if params[:search]
     	@peliculas = Pelicula.search(params[:search])
     elsif	params[:search_name]
       @peliculas = Pelicula.search_name(params[:search_name]).order('created_at DESC')
     elsif params[:search_year]
-      @peliculas = Pelicula.search_year(params[:search_year])
+      @peliculas = Pelicula.search_year(params[:search_year]).order('created_at DESC')
     else
       @peliculas = Pelicula.all.order('created_at DESC')
-    end
-  end
-
-  def show
-    @pelicula = Pelicula.find(params[:id])
   end
 
   def about
   end
+end
 end
