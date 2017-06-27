6e6cfc51782eadc457d3858e1328731050614469
Evarist Jaume <evaristjaumet@gmail.com>
2017-06-27 20:43:18 +0200

    funciona

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,5 @@
 class Setting < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   #validates :country, presence: { message: :no_country}
   #validates :language, presence: { message: :no_language}
   #validates :country, length: { minimum: 2, message: :too_short }
