cb2a732060d670631d48a609f53f77310e973135
Evarist Jaume <evaristjaumet@gmail.com>
2017-06-29 23:12:23 +0200

    almohadilla2

--- db/migrate/20170611113444_add_index_to_topmovies.rb
+++ db/migrate/20170611113444_add_index_to_topmovies.rb
@@ -1,6 +1,6 @@
 class AddIndexToTopmovies < ActiveRecord::Migration[5.1]
   def change
-    add_column :topmovies, :user_id, :integer
+    #add_column :topmovies, :user_id, :integer
     add_index :topmovies, :user_id
   end
 end
